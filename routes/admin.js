26fb716650e939973f3226f3ef72b8bc968bf844
Duke-2406 <deepaksihare891@gmail.com>
2024-09-30 01:26:18 +0530

    File Structure

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,6 @@ router.post('/add-product',
         .isString()
         .isLength({min:3})
         .trim(),
-        body('imageUrl').isURL(),
         body('price').isFloat(),
         body('description')
         .isAlphanumeric()
@@ -39,7 +38,6 @@ router.post('/edit-product',
         .isString()
         .isLength({min:3})
         .trim(),
-        body('imageUrl').isURL(),
         body('price').isFloat(),
         body('description')
         .isAlphanumeric()
